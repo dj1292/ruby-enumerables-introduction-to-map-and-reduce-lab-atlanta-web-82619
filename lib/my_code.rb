24cebc7a56d1eeb44cd7a7058017cc6df5cf2132
Dan Reeves <djreeves1292@gmail.com>
2019-08-18 23:25:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,7 +60,7 @@ end
  
  def reduce_to_any_true(source_array)
    i = 0 
-   while i < source_array[i] do 
+   while i <= source_array[i] do 
      return true if source_array
      i += 1 
    end 
